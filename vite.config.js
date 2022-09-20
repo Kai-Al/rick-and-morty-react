8351725ad96fca617fc3ce497476e6e3ed72f36e
Kai-Al <Kai.albornoz@udea.edu.co>
2022-09-19 20:43:24 -0500

    Ajustando

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://Kai-Al.github.io/react-rick-and-morty/",
+  base: "https://Kai-Al.github.io/rick-and-morty-react/",
   plugins: [react()]
 })
